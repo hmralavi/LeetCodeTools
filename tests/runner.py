ccc4f06870e6eeadf9ad92e86fdcc3da2147e2eb
hmralavi <hmralavi@gmail.com>
2024-03-30 19:53:18 +0330

    removed __init__.py from tests folder

--- tests/runner.py
+++ tests/runner.py
@@ -1,9 +1,12 @@
 import unittest
+import sys
+
+sys.path.append(".")
 
 
 def run_tests():
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover(".", pattern="test_*.py")
+    test_suite = test_loader.discover("tests", pattern="test_*.py")
     test_runner = unittest.TextTestRunner()
     test_runner.run(test_suite)
 
